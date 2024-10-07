b576693c84c46bd201d0e6c56dc54cdf453afe08
Farkas <farasz@kkszki.hu>
2024-10-07 12:10:21 +0200

    Model and database are ready to use.

--- UsersApi/obj/Debug/net8.0/UsersApi.AssemblyInfo.cs
+++ UsersApi/obj/Debug/net8.0/UsersApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsersApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d8a8a501cd740e2cb5b0807d150e727a64f7085")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsersApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsersApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
